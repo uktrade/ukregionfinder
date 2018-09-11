b7060bb5617fcbc8f99ee98d54d52aa4f59a0a06
jimsmith <jim.smith@gmail.com>
2018-09-11 15:49:48 +0100

    Main code

---
+++ server.js
@@ -0,0 +1,27 @@
+const http = require('http');
+
+const port = process.env['PORT'] || 8888;
+
+const {
+  loadRegions,
+  findPostcode
+} = require('./lib/postcodeToRegion')
+
+loadRegions()
+
+const server = http.createServer((req, res) => {
+  let message = 'usage: pcode=POSTCODE'
+
+  if(req.url.indexOf('/pcode') === 0) {
+    let pcode = req.url.split('=')[1]
+    message = findPostcode(pcode)
+  }
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(message);
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
