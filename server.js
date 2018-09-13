eab14cbd5c5421a6a8c070f3c9d4c9f2a5860043
jimsmith <jim.smith@gmail.com>
2018-09-13 14:55:58 +0100

    Added logging

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const http = require('http');
-
-const port = process.env['PORT'] || 8888;
-
 const {
   loadRegions,
   findRegion
 } = require('./lib/postcodeToRegion')
+const log = require('./lib/logger').log
+
+const port = process.env['PORT'] || 8888;
 
 loadRegions()
 
@@ -15,11 +15,13 @@ const server = http.createServer((req, res) => {
   if (req.url.indexOf('/pcode') === 0) {
     let pcode = req.url.split('=')[1]
     message = findRegion(pcode)
+    log(req.url, `responded with ${message}`)
   }
 
   if (req.url.indexOf('/status') === 0) {
     message = 'OK'
   }
+
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
